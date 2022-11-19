edec4b9516a09c1e13044332af5be09925667de1
Yvonne213 <hywener@gmail.com>
2022-11-18 18:16:23 -0600

    create server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.argv[2] || 33
+// do the port thing
+
+const www = express.static(`${__dirname}/www`)
+app.use(www)
+
+app.listen(port, () => {
+    console.log(`running the server on port 13! visit http://locoalhost:${port}`)
+})
