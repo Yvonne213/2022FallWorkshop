47c3bda6f1fa3c05220cba5adba84e94a475cc03
Yvonne213 <hywener@gmail.com>
2022-11-18 19:14:17 -0600

    change port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.argv[2] || 3000
+const port = process.argv[2] || 80
 // do the port thing
 
 const www = express.static(`${__dirname}/www`)
